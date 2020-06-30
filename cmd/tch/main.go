81817e93c01397aefd308b4a5208a101f1ffd237
Guilherme Paixão <guiferpa@gmail.com>
2020-06-30 03:57:00 -0300

    Created all support for suntech protocol

--- cmd/tch/main.go
+++ cmd/tch/main.go
@@ -8,7 +8,7 @@ import (
 	"log"
 	"net"
 	"os"
-	"strings"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -32,13 +32,49 @@ func scanStdin(msgCh chan *bytes.Buffer) {
 	}
 }
 
-func scanTCPServer(conn net.Conn, msgFromServerCh chan *bytes.Buffer) {
+func dateToString(n time.Time) (string, string, string) {
+	year, month, day := n.Date()
+
+	nyear := fmt.Sprintf("%v", year)
+
+	nmonth := fmt.Sprintf("%v", int(month))
+	if int(month) < 10 {
+		nmonth = fmt.Sprintf("0%v", int(month))
+	}
+
+	nday := fmt.Sprintf("%v", day)
+	if day < 10 {
+		nday = fmt.Sprintf("0%v", day)
+	}
+
+	return nyear, nmonth, nday
+}
+
+func clockToString(n time.Time) (string, string, string) {
+	hour, minute, second := n.UTC().Clock()
+
+	nhour := fmt.Sprintf("%v", hour)
+	if hour < 10 {
+		nhour = fmt.Sprintf("0%v", hour)
+	}
+
+	nminute := fmt.Sprintf("%v", minute)
+	if minute < 10 {
+		nminute = fmt.Sprintf("0%v", minute)
+	}
+
+	nsecond := fmt.Sprintf("%v", second)
+	if second < 10 {
+		nsecond = fmt.Sprintf("0%v", second)
+	}
+
+	return nhour, nminute, nsecond
 }
 
 func main() {
 	flag.StringVar(&id, "id", "", "id or IMEI")
 	flag.StringVar(&hostip, "ip", "127.0.0.1", "set a custom host ip")
-	flag.StringVar(&port, "port", "5055", "set a custom port")
+	flag.StringVar(&port, "port", "5011", "set a custom port")
 
 	flag.Parse()
 
@@ -61,9 +97,9 @@ func main() {
 	reader := bufio.NewReader(conn)
 	go func() {
 		for {
-			bs, err := reader.ReadBytes('\n')
+			bs, err := reader.ReadBytes('\r')
 			if err != nil {
-				log.Println("err from server:", err)
+				panic(err)
 			}
 
 			msgFromServerCh <- bytes.NewBuffer(bs)
@@ -78,21 +114,29 @@ func main() {
 			log.Print(msg)
 
 		case msg := <-msgCh:
-			httpRequestFormat := `GET /?%s HTTP/1.1
-Host: %s
-Connection: keep-alive
-User-agent: Traccar Channel
-
-			`
-
 			commandsScanned := fmt.Sprintf("id=%s %s", id, msg.String())
 			log.Printf("Commands scanned: %s\n", commandsScanned)
 
-			query := strings.Join(strings.Split(commandsScanned, " "), "&")
-			httpRequest := fmt.Sprintf(httpRequestFormat, query, addr)
+			n := time.Now()
+			year, month, day := dateToString(n)
+			hour, minute, second := clockToString(n)
 
-			message := bytes.NewBufferString(httpRequest)
-			if _, err := conn.Write(message.Bytes()); err != nil {
+			latitude := 0
+			longitude := 0
+			speed := 0
+			course := 0
+			satellites := 0
+			isValid := 0
+			odometer := 0
+			power := 1
+			status := 0
+			index := 0
+
+			tmpl := "SA200STT;%v;02;%s%s%s;%s:%s:%s;%v;%v;%v;%v;%v;%v;%v;%v;0;%v;%v;0;\r"
+			message := fmt.Sprintf(tmpl, id, year, month, day, hour, minute, second, latitude, longitude, speed, course, satellites, isValid, odometer, power, status, index)
+			buffer := bytes.NewBufferString(message)
+
+			if _, err := conn.Write(buffer.Bytes()); err != nil {
 				log.Println(err)
 				return
 			}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/guiferpa/traccar-channel
 
 go 1.13
 
-require github.com/google/uuid v1.1.1
+require (
+	github.com/google/uuid v1.1.1
+	golang.org/x/text v0.3.3
+)
